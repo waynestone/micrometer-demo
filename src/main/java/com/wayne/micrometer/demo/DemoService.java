7a14d93cde43170746859828113839ad1b31a8c6
石永威 <syw0322@163.com>
2022-07-25 22:50:42 +0800

    test

--- src/main/java/com/wayne/micrometer/demo/DemoService.java
+++ src/main/java/com/wayne/micrometer/demo/DemoService.java
@@ -2,22 +2,34 @@ package com.wayne.micrometer.demo;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class DemoService {
     Counter visitCounter;
-
+    Gauge gauge;
     public DemoService(MeterRegistry registry) {
         visitCounter = Counter.builder("visit_counter")
                 .description("Number of visits to the site")
+                .register(registry);
+
+        gauge = Gauge.builder("number_gauge", this::gaugeValue)
+                .description("test")
                 .register(registry);
     }
 
     public String visit() {
         visitCounter.increment();
         return "Hello World!";
+    }
+
+    public Double gaugeValue(){
+        return 0D;
     }
 
 }
